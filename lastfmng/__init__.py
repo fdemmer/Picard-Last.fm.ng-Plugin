2328c5a88cdb01cc46c08cdffdc79b1e2697ecd3
Florian Demmer <fdemmer@gmail.com>
2015-02-24 20:15:05 +0100

    set version 0.7.4
    this is a service release fixing an issue
    on windows and macos, by disabling the
    collect_unused feature, which required sqlite.
    
    there are some formatting corrections and
    the LICENSE was added.

--- lastfmng/__init__.py
+++ lastfmng/__init__.py
@@ -63,7 +63,7 @@ PLUGIN_AUTHOR = "Florian Demmer"
 PLUGIN_DESCRIPTION = "reimagination of the popular last.fm plus plugin"
 PLUGIN_LICENSE = "GPL-3.0"
 PLUGIN_LICENSE_URL = "http://opensource.org/licenses/GPL-3.0"
-PLUGIN_VERSION = "0.7.3"
+PLUGIN_VERSION = "0.7.4"
 PLUGIN_API_VERSIONS = ["0.15"]
 
 import os
