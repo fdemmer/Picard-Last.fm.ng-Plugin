26e6ef9272beddfbf4a86784d84731fa06bdaa54
Florian Demmer <fdemmer@gmail.com>
2015-08-25 11:15:59 +0200

    Move plugin meta

--- lastfmng/__init__.py
+++ lastfmng/__init__.py
@@ -58,13 +58,8 @@ In addition the special characters `*|:"<>?` are replaced by `~` in the whole
 string.
 """
 
-PLUGIN_NAME = "Last.fm.ng"
-PLUGIN_AUTHOR = "Florian Demmer"
-PLUGIN_DESCRIPTION = "reimagination of the popular last.fm plus plugin"
-PLUGIN_LICENSE = "GPL-3.0"
-PLUGIN_LICENSE_URL = "http://opensource.org/licenses/GPL-3.0"
-PLUGIN_VERSION = "0.7.5"
-PLUGIN_API_VERSIONS = ["0.15"]
+from .meta import *
+
 
 import os
 import operator
---
+++ lastfmng/meta.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+PLUGIN_NAME = "Last.fm.ng"
+PLUGIN_AUTHOR = "Florian Demmer"
+PLUGIN_DESCRIPTION = "reimagination of the popular last.fm plus plugin"
+PLUGIN_LICENSE = "GPL-3.0"
+PLUGIN_LICENSE_URL = "http://opensource.org/licenses/GPL-3.0"
+PLUGIN_VERSION = "0.7.5"
+PLUGIN_API_VERSIONS = ["0.15"]
