ccf6f649ffaa932c46cc290985f9e21c9d44db76
Florian Demmer <fdemmer@gmail.com>
2015-08-26 14:19:00 +0200

    Add PLUGIN_URL to meta

--- lastfmng/meta.py
+++ lastfmng/meta.py
@@ -3,8 +3,15 @@ from __future__ import unicode_literals
 
 PLUGIN_NAME = "Last.fm.ng"
 PLUGIN_AUTHOR = "Florian Demmer"
-PLUGIN_DESCRIPTION = "reimagination of the popular last.fm plus plugin"
 PLUGIN_LICENSE = "GPL-3.0"
 PLUGIN_LICENSE_URL = "http://opensource.org/licenses/GPL-3.0"
 PLUGIN_VERSION = "0.8"
 PLUGIN_API_VERSIONS = ["0.15"]
+
+# unofficial field
+PLUGIN_URL = "https://github.com/fdemmer/Picard-Last.fm.ng-Plugin"
+
+PLUGIN_DESCRIPTION = ("Re-imagination of the popular Last.fm.Plus plugin."
+                      "<br/><br/>"
+                      "<b>Source:</b> {}<br/>"
+                      "<b>License:</b> {}").format(PLUGIN_URL, PLUGIN_LICENSE)
