4da5f7aad7dd67907826f6b29a76c0aa3e299cd6
Florian Demmer <fdemmer@gmail.com>
2017-06-14 16:25:05 +0200

    Remove vendored OrderedDict

--- lastfmng/compat.py
+++ lastfmng/compat.py
@@ -5,11 +5,6 @@ from picard import PICARD_VERSION
 from picard.webservice import XmlWebService
 
 try:
-    from collections import OrderedDict
-except ImportError:
-    from .vendor.odict import OrderedDict
-
-try:
     from ConfigParser import ConfigParser, NoOptionError
 except ImportError:
     from .vendor.ConfigParser import ConfigParser, NoOptionError
