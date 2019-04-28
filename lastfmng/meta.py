f805def17dbd8a3c7397c4370873b3788c643cbb
Florian Demmer <fdemmer@gmail.com>
2019-04-28 12:44:17 +0200

    Change format to single quotes

--- lastfmng/meta.py
+++ lastfmng/meta.py
@@ -1,15 +1,15 @@
-PLUGIN_NAME = "Last.fm.ng"
-PLUGIN_AUTHOR = "Florian Demmer"
-PLUGIN_LICENSE = "GPL-3.0"
-PLUGIN_LICENSE_URL = "http://opensource.org/licenses/GPL-3.0"
-PLUGIN_VERSION = "2.0.2"
-PLUGIN_API_VERSIONS = ["2.0"]
+PLUGIN_NAME = 'Last.fm.ng'
+PLUGIN_AUTHOR = 'Florian Demmer'
+PLUGIN_LICENSE = 'GPL-3.0'
+PLUGIN_LICENSE_URL = 'http://opensource.org/licenses/GPL-3.0'
+PLUGIN_VERSION = '2.0.2'
+PLUGIN_API_VERSIONS = ['2.0']
 
 # unofficial field
-PLUGIN_URL = "https://github.com/fdemmer/Picard-Last.fm.ng-Plugin"
+PLUGIN_URL = 'https://github.com/fdemmer/Picard-Last.fm.ng-Plugin'
 
 PLUGIN_DESCRIPTION = (
-    "Re-imagination of the popular Last.fm.Plus plugin."
-    "<br/><br/>"
-    "<b>Source:</b> {}<br/>"
+    'Re-imagination of the popular Last.fm.Plus plugin.'
+    '<br/><br/>'
+    '<b>Source:</b> {}<br/>'
 ).format(PLUGIN_URL)
